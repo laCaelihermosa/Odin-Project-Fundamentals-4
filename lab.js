293b394258980a8fc43c4719af44468cceba51a0
laCaelihermosa <fabian_lr0505@outlook.com>
2023-03-30 06:53:04 -0600

    files

---
+++ lab.js
@@ -0,0 +1,28 @@
+
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[0] = "Opel";
+// cars[1] = "Audi";
+
+// console.log(cars);
+
+//console doesn't goes with (""), for display in console is console.log(cars);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits[fruits.length - 1];
+// console.log(fruit);
+
+// let i = 0;
+// while (i < 3) { // shows 0, then 1, then 2
+//   alert( i );
+//   i++;
+// }
+
+// let i = 0;
+// while (++i < 5) alert( i );  //1,2,3,4
+
+// let i = 0;
+//  while (i++ < 5) alert( i ); //1,2,3,4,5
+
+
+for(let i = 2; i < 10; i++) aler(i);
---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict'
+// WAYS TO DECLARE AN ARRAY
+
+const carsStore = ["Saab", "Volvo", "BMW"];          //good
+
+//Spaces and line breaks are not important. A declaration can span multiple lines:
+
+const cars = [                                  //good
+    "Toyoda",
+    "MG",
+    "Audi"
+  ];
+
+//You can also create an array, and then provide the elements:
+
+let myArray = [];                                //good
+    car[0] = "carrokk";
+    car[1] = "NISSAN";
+    car[3] = "Volvio";
+
+// let myArray = [          
+//     car[0] = "Saab";
+//     car[1] = "NISSAN";
+//     car[3] = "Volvo";
+// ];                                            // NOT good
